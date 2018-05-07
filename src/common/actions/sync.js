01b63fb86cfde44b7b6bcbe8c10aa7a759d5cfb9
Kiryl Parfiankou <kiryl.parfiankou.dev@gmail.com>
2018-05-07 16:41:11 +0300

    Added and simple test for Flow static type checking

--- src/common/actions/sync.js
+++ src/common/actions/sync.js
@@ -1,21 +1,22 @@
+// @flow
 export const LOAD_SERVICES_REQUEST = 'LOAD_SERVICES_REQUEST';
 export const LOAD_SERVICES_SUCCESS = 'LOAD_SERVICES_SUCCESS';
 export const LOAD_SERVICES_FAIL = 'LOAD_SERVICES_FAIL';
 
-export function loadServicesRequest() {
+export function loadServicesRequest(): { type: string } {
     return {
         type : LOAD_SERVICES_REQUEST
     }
 }
 
-export function loadServicesSuccess(data) {
+export function loadServicesSuccess(data: mixed): { type: string, data: mixed } {
     return {
         type : LOAD_SERVICES_SUCCESS,
         data: data
     }
 }
 
-export function loadServicesFail(data) {
+export function loadServicesFail(data: mixed): { type: string, data: mixed } {
     return {
         type: LOAD_SERVICES_FAIL,
         data: data
--- src/common/api.js
+++ src/common/api.js
@@ -1,11 +1,14 @@
+// @flow
 import base64 from 'base-64';
+import avalibaleProviders from '../';
+
 const DATA_URL = 'http://localhost:9000/api';
 
 function fetchServices() {
     return fetch(`${DATA_URL}/services`);
 }
 
-function createProvider(form) {
+function createProvider(form: mixed) {
     return fetch(`${DATA_URL}/providers`, {
         method: 'POST',
         headers: {
@@ -17,14 +20,14 @@ function createProvider(form) {
     });
 }
 
-function verifyProvider(verificationKey) {
+function verifyProvider(verificationKey: string) {
     return fetch(`${DATA_URL}/providers/verify/${verificationKey}`, {
         method: 'POST',
         cache: 'no-cache'
     });
 }
 
-function login({email, password}) {
+function login({email, password} : {email: string, password: string}) {
     const creds = base64.encode(`${email}:${password}`);
     return fetch(`${DATA_URL}/auth`, {
         method: 'POST',
@@ -32,6 +35,10 @@ function login({email, password}) {
             'Authorization': `Basic ${creds}`
         },
     });
+}
+
+function fetchAvalibaleProviders(form) {
+    return null;
 }
 
 export default {
